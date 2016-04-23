b7c8b8f9b8d4fc2e4d1a24665598a1a19afe8b6b
alfavre <alban.favre@epfl.ch>
2016-04-23 12:27:04 +0200

    player painter fini et testé

--- src/ch/epfl/xblast/server/graphics/PlayerPainter.java
+++ src/ch/epfl/xblast/server/graphics/PlayerPainter.java
@@ -8,9 +8,10 @@ public final class PlayerPainter {
     private PlayerPainter() {
     }
 
-    public static byte byteForPlayer(int tick, Player player) {
+    public static byte byteForPlayer( Player player,int tick) {
         int tempValue = 0;
         
+        //define the correct character sprite group
         switch (player.id().ordinal()) {
         case 0:
         default:
@@ -26,39 +27,52 @@ public final class PlayerPainter {
             break;
         }
 
+        //selects one of the 2 hit sprite
         if (!player.lifeState().canMove())
-            tempValue += (player.lifeState().state().equals(State.DEAD)) ? 13
-                    : 12;
+            tempValue += (player.lifeState().state().equals(State.DYING)) ? ((player.lives() <= 1)?13:12)
+                    : 15;
 
+            
         else {
-            int sprite=0;
+
+            //select the correct direction sprite sub-group and select the correct one
             switch (player.direction()) {
             case N:
             default:
-                sprite=player.position().y()%4;
+                tempValue+=walkingAnimation(player.position().y());
                 break;
             case E:
-                sprite=player.position().x()%4;
-                tempValue += 3;
+
+                tempValue += 3+walkingAnimation(player.position().x());
                 break;
             case S:
-                sprite=player.position().y()%4;
-                tempValue += 6;
+
+                tempValue += 6+walkingAnimation(player.position().y());
                 break;
             case W:
-                sprite=player.position().x()%4;
-                tempValue += 9;
+                tempValue += 9+walkingAnimation(player.position().x());
                 break;
 
             }
             
-            if(){
-                
+            //if the character is invulnerable and the tick is odd, the character sprite will be replaced by the white character
+            if(player.lifeState().state().equals(State.INVULNERABLE)&&tick%2==1){
+                tempValue=(tempValue%20)+80;
             }
             
             
         }
-        return 0;
+        return (byte)tempValue;
+    }
+    
+    /**
+     * It choooses the correct walking animation
+     * @param value
+     *          the value that entirely determines in which walking states the player is
+     * @return if value mod 4 is 0 or 2, it returns 0, if it's 1, it returns 1 and if it's 3, it returns 2
+     */
+    private static int walkingAnimation(int value){
+        return ((value%4)%2==0)?0:(value%4==1?1:2);
     }
 
 }
